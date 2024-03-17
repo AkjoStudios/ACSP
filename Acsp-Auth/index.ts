94d3fd94b8742b5ce29bcc36c9e266cea092c35c
Lukas Küffer <lukas.kueffer@outlook.com>
2024-03-17 16:53:23 +0100

    Added metrics to the ACSP Auth service

--- Acsp-Auth/index.ts
+++ Acsp-Auth/index.ts
@@ -2,12 +2,29 @@ import express from "express";
 import cors from "cors";
 import supertokens from "supertokens-node";
 import { middleware, errorHandler } from "supertokens-node/framework/express";
+import promBundle from "express-prom-bundle";
+import basicAuth from "express-basic-auth";
 
 import { getWebsiteDomain, SuperTokensConfig } from "./config";
 
 
 supertokens.init(SuperTokensConfig)
 const app = express();
+
+const metricsMiddleware = promBundle({
+    includeMethod: true,
+    includePath: true,
+    includeStatusCode: true,
+    includeUp: true,
+    customLabels: {app_name: "Acsp-Auth"},
+    promClient: {
+        collectDefaultMetrics: {}
+    },
+});
+app.use('/metrics', basicAuth({
+    users: { 'agent_user': process.env.GRAFANA_AGENT_TOKEN ?? '' },
+}));
+app.use(metricsMiddleware);
 
 app.use(
     cors({
