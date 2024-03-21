0e4fd39c9805c13efad2e29dfa8605c988999938
Lukas Küffer <lukas.kueffer@outlook.com>
2024-03-21 14:30:42 +0100

    Updated security config quite a bit, so it should work now.

--- Acsp-Backend/src/main/java/com/akjostudios/acsp/backend/security/SecurityConfig.java
+++ Acsp-Backend/src/main/java/com/akjostudios/acsp/backend/security/SecurityConfig.java
@@ -6,6 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -16,10 +20,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -41,8 +47,23 @@ public class SecurityConfig {
         return defaultSecurity(http)
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/api/**").authenticated()
-                        .pathMatchers("/actuator/prometheus").hasRole("PROMETHEUS")
                         .anyExchange().permitAll()
+                ).httpBasic(ServerHttpSecurity.HttpBasicSpec::disable).build();
+    }
+
+    @Bean
+    @Order(0)
+    public @NotNull SecurityWebFilterChain actuatorFilterChain(
+            @NotNull ServerHttpSecurity http,
+            @NotNull @Qualifier("basicAuthenticationManager") ReactiveAuthenticationManager authenticationManager
+    ) {
+        return defaultSecurity(http)
+                .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/actuator/**"))
+                .authorizeExchange(exchanges -> exchanges
+                        .matchers(EndpointRequest.to(PrometheusScrapeEndpoint.class)).hasRole("PROMETHEUS")
+                        .anyExchange().permitAll()
+                ).httpBasic(httpBasic -> httpBasic
+                        .authenticationManager(authenticationManager)
                 ).build();
     }
 
@@ -87,20 +108,24 @@ public class SecurityConfig {
         }
     }
 
-    @Bean
-    public ReactiveAuthenticationManager authenticationManager() {
-        return new UserDetailsRepositoryReactiveAuthenticationManager(
-                new MapReactiveUserDetailsService(
-                        User.withUsername(securityProperties.getPrometheus().getUsername())
-                                .password("{noop}" + securityProperties.getPrometheus().getPassword())
-                                .roles("PROMETHEUS")
-                                .build()
-                )
-        );
+    @Bean("basicAuthenticationManager")
+    public @NotNull ReactiveAuthenticationManager basicAuthenticationManager(
+            @NotNull @Qualifier("prometheusUserDetailsService") ReactiveUserDetailsService userDetailsService
+    ) { return new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService); }
+
+    @Bean("prometheusUserDetailsService")
+    public @NotNull ReactiveUserDetailsService userDetailsService(
+            @NotNull PasswordEncoder passwordEncoder
+    ) {
+        return new MapReactiveUserDetailsService(User
+                .withUsername(securityProperties.getPrometheus().getUsername())
+                .password(passwordEncoder.encode(securityProperties.getPrometheus().getPassword()))
+                .roles("PROMETHEUS")
+                .build());
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public @NotNull PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
