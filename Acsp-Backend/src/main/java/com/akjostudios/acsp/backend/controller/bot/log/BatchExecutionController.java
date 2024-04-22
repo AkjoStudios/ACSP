7df87e1c4d9646cffbb1688652973feeea6508ff
Lukas Küffer <lukas.kueffer@outlook.com>
2024-04-22 20:07:42 +0200

    Added swagger / openapi to backend service and other small improvements

--- Acsp-Backend/src/main/java/com/akjostudios/acsp/backend/controller/bot/log/BatchExecutionController.java
+++ Acsp-Backend/src/main/java/com/akjostudios/acsp/backend/controller/bot/log/BatchExecutionController.java
@@ -1,8 +1,5 @@
 package com.akjostudios.acsp.backend.controller.bot.log;
 
-import com.akjostudios.acsp.backend.data.repository.bot.log.batch.BatchExecutionRepository;
-import com.akjostudios.acsp.backend.data.repository.bot.log.batch.BatchInteractionRepository;
-import com.akjostudios.acsp.backend.data.repository.bot.log.batch.BatchResponseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bot/log/batch")
 @Secured({"SERVICE_BOT"})
 @RequiredArgsConstructor
-public class BatchExecutionController {
-    private final BatchExecutionRepository executionRepository;
-    private final BatchResponseRepository responseRepository;
-    private final BatchInteractionRepository interactionRepository;
-}
+public class BatchExecutionController {}
--- Acsp-Backend/src/main/java/com/akjostudios/acsp/backend/controller/bot/log/CommandExecutionController.java
+++ Acsp-Backend/src/main/java/com/akjostudios/acsp/backend/controller/bot/log/CommandExecutionController.java
@@ -1,8 +1,5 @@
 package com.akjostudios.acsp.backend.controller.bot.log;
 
-import com.akjostudios.acsp.backend.data.repository.bot.log.command.CommandExecutionRepository;
-import com.akjostudios.acsp.backend.data.repository.bot.log.command.CommandInteractionRepository;
-import com.akjostudios.acsp.backend.data.repository.bot.log.command.CommandResponseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bot/log/command")
 @Secured({"SERVICE_BOT"})
 @RequiredArgsConstructor
-public class CommandExecutionController {
-    private final CommandExecutionRepository executionRepository;
-    private final CommandResponseRepository responseRepository;
-    private final CommandInteractionRepository interactionRepository;
-}
+public class CommandExecutionController {}
